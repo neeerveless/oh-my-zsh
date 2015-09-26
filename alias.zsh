76cf71486eaa8f01582a41eb8d9d2f4e6f4b5574
mshr <mshr@localhost.localdomain>
2015-09-27 01:25:20 +0900

    swapコマンド作成

--- alias.zsh
+++ alias.zsh
@@ -8,6 +8,23 @@ alias start='./script/*_server.pl -r -p 3004'
 alias create='./script/*_create.pl'
 alias mp='measureperl'
 alias repl='perl -le "use Carp::Reply qw();Carp::Reply::repl();"'
+swap() {
+  if [ $# -ne 2 ]; then
+    echo "select two file!";
+    return;
+  fi
+  if [ -d $1 -o -d $2 ]; then
+    echo "can not use dir!";
+    return;
+  fi
+  if [ ! -f $1 -o ! -f $2 ]; then
+    echo "file not found!";
+    return;
+  fi
+  mv $1 tmp;
+  mv $2 $1;
+  mv tmp $2;
+}
 startc() { ./script/*_server.pl -r -p $1; }
 plackc() { plackup -p $1 -e 'use Plack::App::Directory; Plack::App::Directory->new({root => "./cover_db"})->to_app'; }
 cd ()
